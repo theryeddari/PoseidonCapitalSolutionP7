910f7027a8e7187e82bc4caf51bfc5212ea715bc
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-09 16:24:27 +0200

    add Getter and setter and write entity BidList, refactor test and valid CRUD on it

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,11 +1,102 @@
 package com.nnk.springboot.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
 
 
 @Entity
 @Table(name = "bidlist")
+@Getter
+@Setter
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BidListId")
+    private Integer bidListId;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "account", nullable = false, length = 30)
+    private String account;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "type", nullable = false, length = 30)
+    private String type;
+
+    @Column(name = "bidQuantity")
+    private Double bidQuantity;
+
+    @Column(name = "askQuantity")
+    private Double askQuantity;
+
+    @Column(name = "bid")
+    private Double bid;
+
+    @Column(name = "ask")
+    private Double ask;
+
+    @Size(max = 125)
+    @Column(name = "benchmark", length = 125)
+    private String benchmark;
+
+    @Column(name = "bidListDate")
+    private Instant bidListDate;
+
+    @Size(max = 125)
+    @Column(name = "commentary", length = 125)
+    private String commentary;
+
+    @Size(max = 125)
+    @Column(name = "security", length = 125)
+    private String security;
+
+    @Size(max = 10)
+    @Column(name = "status", length = 10)
+    private String status;
+
+    @Size(max = 125)
+    @Column(name = "trader", length = 125)
+    private String trader;
+
+    @Size(max = 125)
+    @Column(name = "book", length = 125)
+    private String book;
+
+    @Size(max = 125)
+    @Column(name = "creationName", length = 125)
+    private String creationName;
+
+    @Column(name = "creationDate")
+    private Instant creationDate;
+
+    @Size(max = 125)
+    @Column(name = "revisionName", length = 125)
+    private String revisionName;
+
+    @Column(name = "revisionDate")
+    private Instant revisionDate;
+
+    @Size(max = 125)
+    @Column(name = "dealName", length = 125)
+    private String dealName;
+
+    @Size(max = 125)
+    @Column(name = "dealType", length = 125)
+    private String dealType;
+
+    @Size(max = 125)
+    @Column(name = "sourceListId", length = 125)
+    private String sourceListId;
+
+    @Size(max = 125)
+    @Column(name = "side", length = 125)
+    private String side;
+
 }
