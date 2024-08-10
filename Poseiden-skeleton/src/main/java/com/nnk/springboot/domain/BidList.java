f18c8746ef6600be87312f4501e86268c347d00b
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-10 16:40:54 +0200

    delete h2 dependency, refactor fix and clean data sql and BidList entity (add an entry bidList for test)!add naming convention for name of column in properties for hibernate!

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,32 +1,25 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
-
-@Entity
-@Table(name = "bidlist")
 @Getter
 @Setter
+@Entity
+@Table(name = "bidlist")
 public class BidList {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "BidListId")
-    private Integer bidListId;
+    @Column(name="BidListId", unique = true, nullable = false)
+    private Byte bidListId;
 
-    @Size(max = 30)
-    @NotNull
     @Column(name = "account", nullable = false, length = 30)
     private String account;
 
-    @Size(max = 30)
-    @NotNull
     @Column(name = "type", nullable = false, length = 30)
     private String type;
 
@@ -42,60 +35,48 @@ public class BidList {
     @Column(name = "ask")
     private Double ask;
 
-    @Size(max = 125)
     @Column(name = "benchmark", length = 125)
     private String benchmark;
 
     @Column(name = "bidListDate")
     private Instant bidListDate;
 
-    @Size(max = 125)
     @Column(name = "commentary", length = 125)
     private String commentary;
 
-    @Size(max = 125)
     @Column(name = "security", length = 125)
     private String security;
 
-    @Size(max = 10)
     @Column(name = "status", length = 10)
     private String status;
 
-    @Size(max = 125)
     @Column(name = "trader", length = 125)
     private String trader;
 
-    @Size(max = 125)
     @Column(name = "book", length = 125)
     private String book;
 
-    @Size(max = 125)
     @Column(name = "creationName", length = 125)
     private String creationName;
 
     @Column(name = "creationDate")
     private Instant creationDate;
 
-    @Size(max = 125)
     @Column(name = "revisionName", length = 125)
     private String revisionName;
 
     @Column(name = "revisionDate")
     private Instant revisionDate;
 
-    @Size(max = 125)
     @Column(name = "dealName", length = 125)
     private String dealName;
 
-    @Size(max = 125)
     @Column(name = "dealType", length = 125)
     private String dealType;
 
-    @Size(max = 125)
     @Column(name = "sourceListId", length = 125)
     private String sourceListId;
 
-    @Size(max = 125)
     @Column(name = "side", length = 125)
     private String side;
 
