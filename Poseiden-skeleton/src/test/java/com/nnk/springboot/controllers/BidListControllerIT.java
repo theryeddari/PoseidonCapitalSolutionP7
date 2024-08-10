bea1b5bb45d4bba14381f6761206c2e45e5925c8
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-10 17:58:59 +0200

    Code IT for BidList router home() method and write it: -config permitAll security to use with profile "test" in order to have pass integration test (not login). -Fix static variable for remoteUser [[${}]] to ${} dynamic field into list Thymeleaf !BUT NOT THE SOLUTION!

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerIT.java
@@ -0,0 +1,43 @@
+package com.nnk.springboot.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@WithMockUser(username = "user", roles = "USER")
+public class BidListControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void bidListAggregationInfo() throws Exception {
+
+        mockMvc.perform(get("/bidList/list"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("<td>user</td>")))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("<td>USER</td>")))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("<td>10.0</td>")));
+    }
+
+}
