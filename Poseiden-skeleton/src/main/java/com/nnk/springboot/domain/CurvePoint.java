79dec2e81d5ea03ca77420601c739d148268eb1a
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-12 22:13:14 +0200

    create entity

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,11 +1,32 @@
 package com.nnk.springboot.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
 
 
 @Entity
 @Table(name = "curvepoint")
+@Getter
+@Setter
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CurveId")
+    private Byte curveId;
+
+    @Column(name = "asOfDate")
+    private Instant asOfDate;
+
+    @Column(name = "term")
+    private Double term;
+
+    @Column(name = "value")
+    private Double value;
+
+    @Column(name = "creationDate")
+    private Instant creationDate;
 }
