c20ea15f7f28033f07813f35e19c2d26b0cae98f
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-13 03:24:28 +0200

    add validation @Positive into BidList forget

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -3,6 +3,7 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,6 +34,7 @@ public class BidList {
 
     @Column(name = "bidQuantity")
     @NotNull(message = "{bid-list.type.not-null}")
+    @Positive(message = "{app.positive}")
     private Double bidQuantity;
 
     @Column(name = "askQuantity")
