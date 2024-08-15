11243c7f2f198e9a4863cc2d89e8a553233696bd
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-08-16 00:10:51 +0200

    add logout manage by spring security.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/security/SecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/security/SecurityConfig.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -78,6 +78,11 @@ public class SecurityConfig {
                 .exceptionHandling(exceptions ->
                         exceptions
                                 .accessDeniedPage("/403")  // Redirect to error page on access denial
+                )
+                .logout(formLogout ->
+                        formLogout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/app-logout", "POST"))
+                                .logoutSuccessUrl("/home")
                 );
 
         return http.build();
